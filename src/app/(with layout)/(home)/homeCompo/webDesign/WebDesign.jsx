a25073b5ff1bf0e0eb467624e084ca00ed8f6869
ocircleo <salmanhossain11222626@gmail.com>
2024-02-10 21:19:53 +0600

    home page clean up done

--- src/app/(with layout)/(home)/homeCompo/webDesign/WebDesign.jsx
+++ src/app/(with layout)/(home)/homeCompo/webDesign/WebDesign.jsx
@@ -1,10 +1,6 @@
-"use-client"
-
 import React from 'react';
 import Image from 'next/image'
 import devPic from '../../../../../../public/images/1618.jpg'
-import './webDsign.css'
-
 const WebDesign = () => {
   return (
 
